88984a1e65f12a6811cd3e559ec0f85242ccfe22
Jordan Suero <9836321+jtsuero@users.noreply.github.com>
2019-07-12 20:59:55 -0700

    remove unused functions

--- src/CardPage.js
+++ src/CardPage.js
@@ -1,44 +1,14 @@
 import React, { Component } from 'react';
 import AppStore from './AppStore.js';
 import CardPrinter from './CardPrinter.js';
-import DeckPrinter from './DeckPrinter.js';
 
 class CardPage extends Component {
-  constructor() {
-    super()
-    this.state = {
-      showDecks: false,
-
-    }
-  }
-
-
-  getCards = () => {
-    let cards = AppStore.getCards();
-    return cards;
-  }
-
-  displayDecks = () => {
-    this.setState({showDecks: true})
-  }
-
   render() {
-
-  if(this.state.showDecks) {
-    return(
-      <DeckPrinter decks={AppStore.getDecks()} /> //fix method send to deckPrinter, might have to add new one
-    )
-
-  } else {
     return (
       <div className='card-page'>
-        <CardPrinter cards={AppStore.getCards()} displayDecks={this.displayDecks} /> {/* or does this need to be this.getCards?*/}
-        <div>
-          test {/*add inline style, hidden until clicked,*/}
-        </div>
+        <CardPrinter cards={AppStore.getCards()} />
       </div>
     )
-  }
   }
 }
 
