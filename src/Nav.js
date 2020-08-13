8788386fc88af5412b2f1cdf3c11714c05d49de1
Jordan Suero <9836321+jtsuero@users.noreply.github.com>
2020-08-13 13:37:34 -0700

    fix nav bar styling

--- src/Nav.js
+++ src/Nav.js
@@ -5,6 +5,8 @@ import {Link} from 'react-router-dom';
 function Nav() {
   return (
     <nav>
+      <div className="triangle"></div>
+      <div className="logo">Flashy</div>
       <ul className="nav-links-list">
         <li>
           <Link to="/" className="nav-links">
@@ -16,6 +18,7 @@ function Nav() {
             Decks
           </Link>
         </li>
+        <li className="nav-links">Cards</li>
       </ul>
     </nav>
   );
